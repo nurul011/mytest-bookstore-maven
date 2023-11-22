7773f82aefa92659e9a94c4d5cf91430746f4cdd
Nurul Islam <[200~nurul.raja91@gmail.com>
2023-11-22 13:36:14 +0530

    first commit of servicebook

---
+++ src/test/java/com/nurultest/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurultest.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
